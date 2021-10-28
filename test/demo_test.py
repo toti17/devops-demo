4968e88eb57073410ecc1a50cf81c083f6706f5c
toti17 <charlesaposaga@gmail.com>
2021-10-28 21:48:25 +0800

    feat: gitignore

---
+++ test/demo_test.py
@@ -0,0 +1,21 @@
+import pytest
+from src.demo import add, subtract, multiply, divide
+
+x = 1
+y = 1
+
+def test_add_should_return_correct_sum():
+    sum = x + y
+    assert add(x, y) == sum
+
+def test_subtract_should_return_correct_difference():
+    diff = x - y
+    assert subtract(x, y) == diff
+
+def test_multiply_should_return_correct_product():
+    product = x * y
+    assert multiply(x, y) == product
+
+def test_divide_should_return_correct_quotient():
+    quotient = x / y
+    assert divide(x, y) == quotient
